f27abd1e1e16894c249398132484c277efc370b3
João Marcos Pereira De Souza <112454993+Joaomarcospsouza@users.noreply.github.com>
2022-08-30 01:45:58 -0300

    Criando Tabelas
    Criando Tabela Cidade e Estado

--- DesafioBD.sql
+++ DesafioBD.sql
@@ -2,13 +2,28 @@ DROP DATABASE IF EXISTS aula_bd;
 CREATE DATABASE aula_bd;
 USE aula_bd;
 
+CREATE TABLE estado(
+id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(250) NOT NULL UNIQUE,
+uf CHAR(2) NOT NULL UNIQUE
+);
+
+CREATE TABLE cidade(
+id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(250) NOT NULL UNIQUE,
+estado_id INT NOT NULL,
+FOREIGN key(estado_id) REFERENCES estado(id)
+);
+
 CREATE TABLE fazenda (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(250) NOT NULL UNIQUE,
 rua VARCHAR(250) NOT NULL UNIQUE,
 complemento VARCHAR(100) NOT NULL UNIQUE,
 hectares DOUBLE NOT NULL UNIQUE,
-cep INT(7) NOT NULL UNIQUE
+cep INT(7) NOT NULL UNIQUE,
+cidade_id INT NOT NULL,
+FOREIGN KEY(cidade_id) REFERENCES cidade(id)
 );
 
 CREATE TABLE pessoa (
@@ -18,7 +33,9 @@ cpf INT(11) NOT NULL UNIQUE,
 data_nascimento DATE NOT NULL UNIQUE,
 endereco VARCHAR(250) NOT NULL UNIQUE,
 fazenda_id INT NOT NULL,
-FOREIGN KEY(fazenda_id) REFERENCES fazenda(id)
+cidade_id INT NOT NULL,
+FOREIGN KEY(fazenda_id) REFERENCES fazenda(id),
+FOREIGN KEY(cidade_id) REFERENCES cidade(id)
 );
 
 CREATE TABLE animal (
@@ -30,11 +47,19 @@ fazenda_id INT NOT NULL,
 FOREIGN KEY(fazenda_id) REFERENCES fazenda(id)
 );
 
+/* INSERTS TABELA ESTADO */
+INSERT INTO estado() VALUE ();
+SELECT * FROM estado;
+
+/* INSERTS TABELA CIDADE */
+INSERT INTO estado() VALUE ();
+SELECT * FROM estado;
+
 /* INSERTS TABELA FAZENDA */
-INSERT INTO fazenda(nome, rua, complemento, hectares, cep) VALUE ('Fazenda Santa Matilda', 'Avenida João Alexandre', 'Primeira porteira a esquerda da arvore grande', '78', '52669');
-INSERT INTO fazenda(nome, rua, complemento, hectares, cep) VALUE ('Fazenda Roça', 'Avenida Tome Souza', 'Perto da fabrica de canudo', '120', '98654');
-INSERT INTO fazenda(nome, rua, complemento, hectares, cep) VALUE ('Fazenda Constantinople', 'Rua Cobalto', 'Proximo ao mercado santana', '250', '32148');
-SELECT nome, rua, complemento, hectares, cep FROM fazenda;
+INSERT INTO fazenda(nome, rua, complemento, hectares, cep, cidade) VALUE ('Fazenda Santa Matilda', 'Avenida João Alexandre', 'Primeira porteira a esquerda da arvore grande', '78', '52669', '1');
+INSERT INTO fazenda(nome, rua, complemento, hectares, cep, cidade) VALUE ('Fazenda Roça', 'Avenida Tome Souza', 'Perto da fabrica de canudo', '120', '98654', '2');
+INSERT INTO fazenda(nome, rua, complemento, hectares, cep, cidade) VALUE ('Fazenda Constantinople', 'Rua Cobalto', 'Proximo ao mercado santana', '250', '32148', '3');
+SELECT nome, rua, complemento, hectares, cep, cidade FROM fazenda;
 
 /* INSERTS TABELA PESSOA */
 INSERT INTO pessoa(nome, cpf, data_nascimento, endereco, fazenda_id) VALUE ('João Marcos', '859654987', '1999-04-06', 'Rua Tome Souza', '1');
